4eddbe321eee2cac94ac901d236c418879f45ad3
Marcin Domański <kartofelek007@gmail.com>
2019-10-31 10:46:11 +0100

    przeniesienie zadania z panstwami

---
+++ 5-tablice/5-zadanie-panstwa/js/app.js
@@ -0,0 +1,25 @@
+/*
+Do wykonania tego zadania potrzebujesz mieć dobrze zainstalowany Node.js oraz umieć
+instalować paczki.
+-------------
+
+W pliku data/data.json znajduje się tablica z państwami.
+Zainstaluj na swoim komputerze json-server, a następnie odpal w tym katalogu poleceniem:
+
+json-server --watch data/data.json
+
+Następnie za pomocą ajaxa połącz się na adres który wygeneruje ci odpalony serwer i operując na zwróconych danych wykonaj poniższe polecenia:
+
+1. Wypisz wszystkie nazwy państw (a jak juz wypiszesz to zakomentuj, bo tego jest zbyt dużo)
+2. Wypisz ile jest ludności we wszystkich państwach
+3. Wypisz średnią ludność w państwach
+4. Wypisz ile jest państw, które mają dodatni wzrost
+5. Wypisz ile jest państw, które mają ujemny wzrost
+6. Napisz ile wszystkie państwa zajmują procent powierzchni Ziemi (world_area)
+7. Napisz ile wynosi średnia dzietności na świecie (fertility_rate)
+    (uwaga - niektóre kraje nie mają danych - wtedy jest null)
+8. Napisz ile wynosi średnia wieku na świecie
+9. Wypisz wszystkie dane na temat Polski
+11. Napisz czy średnia wieku w Polsce jest większa od średniej na Świecie?
+
+*/
